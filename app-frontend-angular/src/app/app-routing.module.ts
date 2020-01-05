c72364bfc7e4a2ab1779d7de32181c3aad5ee281
Raidel Berrillo Gonzalez <raidelbg84@gmail.com>
2020-01-05 09:27:15 -0500

    CRUD Contract

--- app-frontend-angular/src/app/app-routing.module.ts
+++ app-frontend-angular/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
+  { path: '', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'public', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
 ];
 
 @NgModule({
